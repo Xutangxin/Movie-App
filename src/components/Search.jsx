c03456930b149f0eecfb87fa0a7fda177f2bb5b6
xutangxin <xutangxin@mindflow.com.cn>
2025-09-05 21:21:15 +0800

    handleEnter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,19 @@
-const Search = ({ searchVal, setSearchVal }) => {
+const Search = ({ searchVal, setSearchVal, query }) => {
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      query();
+    }
+  }
+
   return (
     <div className="search">
       <div>
         <img src="search.svg" alt="" />
-
         <input
           type="input"
           placeholder="What's on your mind ?"
           value={searchVal}
+          onKeyDown={handleKeyDown}
           onChange={(e) => setSearchVal(e.target.value)}
         />
       </div>
